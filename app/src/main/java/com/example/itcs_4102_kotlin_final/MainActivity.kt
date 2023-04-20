ddf4a5483c7c00f8efa0975b0234e6f1e04374eb
maanavvcc <mmehta14@uncc.edu>
2023-04-20 01:39:04 -0400

    2nd commit

--- app/src/main/java/com/example/itcs_4102_kotlin_final/MainActivity.kt
+++ app/src/main/java/com/example/itcs_4102_kotlin_final/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.itcs_4102_kotlin_final
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mAuth = FirebaseAuth.getInstance()
+        if(mAuth.currentUser == null){
+            supportFragmentManager.beginTransaction()
+                .add(R.id.containerView, LoginFragment())
+                .commit()
+        } else {
+            supportFragmentManager.beginTransaction()
+                //.add(R.id.containerView, LoginFragment()) TODO
+                .commit()
+        }
     }
 }
